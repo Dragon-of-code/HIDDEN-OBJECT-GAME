e4a9cf120515119551e8841e8e31c3300cef4452
Dragon-of-code <cesarur2013@gmail.com>
2025-04-02 20:12:22 +0000

    new file:   Images/download (56).jpeg 	new file:   Images/download (57).jpeg 	new file:   Images/mm-bag_grande.webp

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ let object = document.getElementById("object")
 let winMessage = document.getElementById("win")
 let restart = document.getElementById("restart")
 
+object.addEventListener("click", win)
 
+function win(){
+    object.setAttribute("class" , "hidden")
+winMessage.setAttribute("class","")
+    
+}
 
 
